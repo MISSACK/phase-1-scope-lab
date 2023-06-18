f2bd0b001248a8857af8048262c34c77eb9ee42c
Musa Issack <musa.m.issack@gmail.com>
2023-06-18 12:11:27 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
-// Write your solution in this file!
+var customerName = 'bob';
+var bestCustomer;
+
+function upperCaseCustomerName() {
+  if (customerName) {
+    customerName = customerName.toUpperCase();
+  }
+}
+
+function setBestCustomer() {
+  bestCustomer = 'maybe bob'; 
+}
+
+console.log(bestCustomer); 
+
+setBestCustomer(); 
+
+console.log(bestCustomer); 
+
+var customerName = 'bob';
+var bestCustomer;
+const leastFavoriteCustomer = 'John';
+
+function upperCaseCustomerName() {
+  if (customerName) {
+    customerName = customerName.toUpperCase();
+  }
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(newCustomer) {
+  bestCustomer = newCustomer;
+}
+
+function changeLeastFavoriteCustomer() {
+
+  leastFavoriteCustomer = 'Jane';
+}
+
+console.log(bestCustomer); 
+
+setBestCustomer(); 
+
+console.log(bestCustomer); 
+
+overwriteBestCustomer('maybe bob'); 
+
+console.log(bestCustomer); 
+
+console.log(leastFavoriteCustomer); 
+
+changeLeastFavoriteCustomer(); 
+
